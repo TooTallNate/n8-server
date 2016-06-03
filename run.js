2f492a8e4d195d299687a0138e9cf1d4273c3c19
Nathan Rajlich <nathan@tootallnate.net>
2016-06-02 17:25:16 -0700

    Use a wrapper `run.sh` script

--- run.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-// http://stackoverflow.com/a/33454270/376773
-module.exports = require('./build/server');
---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# this script runs the compiled `server.js` file via Node.js,
+# and passes along any arguments passed to the script.
+#
+# it also exports NODE_PATH such that the `node_modules` directory
+# relative to the current working directory will be included in
+# the search path
+
+if [ "x$NODE" = "x" ]; then
+  NODE=node
+fi
+
+# thanks http://stackoverflow.com/a/246128/376773 !
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# TODO: use a module to find the root of the module (node-bindings has this logic, but not exposed)
+export NODE_PATH="$PWD/node_modules:$NODE_PATH"
+
+"$NODE" "$DIR/build/server.js" "$@"
