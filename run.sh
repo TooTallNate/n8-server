b93ad0693c9bff82d1ba761e6b852e5557158136
Nathan Rajlich <nathan@tootallnate.net>
2016-06-03 11:12:50 -0700

    run: simpler and more portable DIR logic

--- run.sh
+++ run.sh
@@ -11,14 +11,8 @@ if [ "x$NODE" = "x" ]; then
   NODE=node
 fi
 
-# thanks http://stackoverflow.com/a/246128/376773 !
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+# thanks http://stackoverflow.com/a/8506790/376773 !
+DIR=$( cd $( dirname -- "$0" ) > /dev/null ; pwd )
 
 # TODO: use a module to find the root of the module
 # (node-bindings has this logic, but it's not exposed)
