f830a6bcea13e5203064fb6b6f78067aa165694c
Nathan Rajlich <nathan@tootallnate.net>
2016-09-27 17:18:57 -0700

    error: fix 404 endpoint message

--- error.js
+++ error.js
@@ -5,7 +5,7 @@ export default async function endpoint (req, res) {
   const code = +req.headers['x-status-code'] || 500;
   const err = new HTTPError(code);
   if (err.statusCode === 404) {
-    const pathname = parse(req.url);
+    const { pathname } = parse(req.url);
     err.message = `API endpoint "${req.method} ${pathname}" does not exist`;
   }
   throw err;
